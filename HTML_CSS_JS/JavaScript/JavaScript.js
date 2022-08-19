f4205292bb0b6bd2c394d1c1e65eb4e319789f03
Mauricio Rodrigues Cerqueira <mauricio.cerqueira@senairs.org.br>
2022-08-19 16:57:38 -0300

    Adicionar arquivos de projeto.

---
+++ HTML_CSS_JS/JavaScript/JavaScript.js
@@ -0,0 +1,32 @@
+﻿const dt = new Date().toLocaleString();
+
+document.getElementById("data").innerHTML = dt;
+
+function FuncaoBotao() {
+
+    var nome = document.getElementById("nomeId");
+    var senha = document.getElementById("senhaId");
+    var email = document.getElementById("emailId");
+    var msg = document.getElementById("msgId");
+    if (nome.value != "" & senha.value != "" & email.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' seu email ' + email.value + ' foi cadastrado com sucesso');
+    } else {
+        alert('Você não preencheu todos os campos obrigatórios');
+    }
+}
+
+function FuncaoFormulario() {
+    var nome = document.getElementById("nomeId");
+    var senha = document.getElementById("senhaId");
+    var email = document.getElementById("emailId");
+    if (nome.value != "" & senha.value != "" & email.value != "") {
+        document.getElementById("confirmacao").innerHTML = "Realize o Cadastro clicando em \"Enviar\"";
+    } else {
+        document.getElementById("confirmacao").innerHTML = "Complete todos os dados";
+    }
+
+}
+
+function FuncaoAviso() {
+    alert('Este é apenas um painel de avisos!');
+}
